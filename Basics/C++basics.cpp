9bef4eb90b9dcc3138536f51e1f832a3b17bb656
Chaitu-15 <holmukhechaitanya15@gmail.com>
2026-08-07 02:11:14 +0530

    Created Basic folder

---
+++ Basics/C++basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    cout<<"Heyy Chaitanya"<<endl;
+    cout<<"Imm very happy"<<endl;
+    cout<<"Yooo";
+    return 0;
+}
