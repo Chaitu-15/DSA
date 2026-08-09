966e2d5be3a7159189e519e2f5208645d1285383
Chaitu-15 <holmukhechaitanya15@gmail.com>
2026-08-10 03:52:08 +0530

    made some progress in basics

---
+++ Basics/BasicsNotes.md
@@ -0,0 +1,8 @@
+#Notes
+
+##getline
+string s;
+   getline(cin,s);
+    cout<<s;
+
+int len = str.size();
--- Basics/C++basics.cpp
+++ Basics/C++basics.cpp
@@ -3,11 +3,40 @@
 #include<string>
 using namespace std;
 
-int main()
-{
-    int x,y;
-    cin>>x;
-    cin>>y;
-    cout<<"Value of x : "<<x<<"  Value of y : "<<y;
-    return 0;
+int main(){
+
+
+for (int i=1;i<=10;i++){
+   cout<<"Chaitanya"<<endl;
 }
+
+
+
+
+return 0;
+
+}
+    /*
+   int age;
+   cin >>age;
+   if(age>=18)
+   {
+    cout<<"You are an adult";
+   }
+   else{
+    cout<<"You are underage";
+   }
+
+ int marks;
+ cin>>marks;
+ if(marks<25){
+    cout<<"F";
+ }
+ else if(marks<=44){ 
+    cout<<"E\n";
+    if(marks>40){
+        cout<<"lucky above avg";
+    }
+ }
+
+}*/
