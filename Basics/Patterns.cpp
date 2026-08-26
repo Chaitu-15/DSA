7880c2ea614f9a38c25f6bba876345bef20865ae
Chaitu-15 <holmukhechaitanya15@gmail.com>
2026-08-27 00:06:28 +0530

    Learnt some patterns

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -70,16 +70,16 @@ for (int i=1;i<=n;i++)
 }
 void pattern7(int n)
 {
-for (int i=0;i<=n;i++)
+for (int i=0;i < n;i++)
     {
-        for(int j=0;j<n-i+1;j++)
+        for(int j=0 ; j < n-i-1 ; j++)
         {
             cout<<" ";
         }
-        for(int j=0;j<2*i+1;j++){
+        for(int j=0 ; j < 2*i+1 ; j++){
             cout<<"*";
         }   
-          for(int j=0;j<n-i+1;j++)
+          for(int j=0 ; j < n-i-1 ; j++)
         {
             cout<<" ";
         }
@@ -94,7 +94,7 @@ for (int i=0;i<n;i++)
         {
             cout<<" ";
         }
-        for(int j=0;j<2*n-(2*i+1);j++){
+        for(int j=0;j<2*(n-i)-1;j++){
             cout<<"*";
         }   
           for(int j=0;j<i;j++)
