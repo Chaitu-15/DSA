ece3c268d2166ee735cef29b7c2a8e2d3f844016
Chaitu-15 <holmukhechaitanya15@gmail.com>
2026-08-28 00:02:00 +0530

    learned some new patterns

--- Basics/Patterns.cpp
+++ Basics/Patterns.cpp
@@ -86,7 +86,7 @@ for (int i=0;i < n;i++)
         cout<<endl;
     }
 }
-void pattern8(int n)
+void pattern8_9(int n)
 {
 for (int i=0;i<n;i++)
     {
@@ -104,14 +104,116 @@ for (int i=0;i<n;i++)
         cout<<endl;
     }
 }
-
+void pattern10(int n)
+{
+    for (int i=1;i<=n*2-1;i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+        for(int j= 1;j<=stars;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void pattern11(int n)
+{
+    int start =1;
+    for (int i =0;i<n; i++){
+        if(i%2==0) start=1;
+        else start =0;
+        for (int j=0;j<=i;j++){
+            cout<<start;
+            start=1-start;//flip the value 0101
+        }
+        cout<<endl; 
+    }
+}
+void pattern12(int n)
+{
+    for(int i=1;i<=n;i++){
+        //number
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        //space
+        for(int j=1;j<=2*n-2*i;j++)
+        {
+            cout<<" ";
+        }
+        //numbers
+         for(int j=i;j>=1;j--)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void pattern13(int n)
+{
+    int cnt=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<cnt;
+            cnt+=1;
+        }
+            cout<<endl;
+    }
+}
+void pattern14(int n)
+{
+    for(int i=0;i<=n;i++){
+        for(char ch='A' ; ch<='A'+i; ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void pattern15(int n)
+{
+    for (int i=0;i<=n;i++){
+        for(char ch = 'A' ; ch <= 'A'+(n-i-1);ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void pattern16(int n)
+{
+    for (int i=0;i<=n;i++){
+        char ch ='A'+i;
+        for(int j=0; j<=i;j++){
+            cout<<ch<<" ";
+            
+        }
+        cout<<endl;
+    }
+}
+void pattern17(int n)
+{
+    for(int i=0;i<=n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //characters
+        char ch='A';
+        for(int j=0 ; j<=n;j++){
+            cout<<ch;
+        }
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int t;
     cin>>t;
     for(int i=0;i<t;i++){
     int n;
     cin >> n;
-    pattern7(n);
-    pattern8(n);
+    pattern17(n);
 }
 }
