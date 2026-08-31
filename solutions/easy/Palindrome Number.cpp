d660da2dd41dc88d933d4d2bade84cd6f4275a49
Chaitanya <holmukhechaitanya15@gmail.com>
2026-08-31 23:43:51 +0530

    Add solution for Palindrome Number

---
+++ solutions/easy/Palindrome Number.cpp
@@ -0,0 +1,23 @@
+// Title: Palindrome Number
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev = 0;
+        int ogX = x;
+        if(x < 0) return false;
+        while(x != 0){
+            int lastdigit  = x%10;
+            rev = rev * 10 + lastdigit;
+            x = x/10;
+        }
+        if (rev == ogX){
+            return true;
+        }
+        else return false;
+        
+    }
+};
