9f20067a869d57a7a39a25ad26f2c0a3b765600e
Chaitu-15 <holmukhechaitanya15@gmail.com>
2026-09-03 23:19:28 +0530

    add recursion

--- Basics/recursion.cpp
+++ Basics/recursion.cpp
@@ -1,16 +1,31 @@
 #include<iostream>
 using namespace std;
-int fact(int n)
+/*int fact(int n)
 {
     if(n==0) return 1;// not return 0 because 0 gets multiplied and final ans becomes 0
 
     return n * fact(n-1);
 }
+*/
+void f(int i , int arr[] ,int n){
+    if(i>=n/2) return;
+    swap(arr[i] , arr[n-i-1]);
+    f(i+1,arr,n);
+}
 int main()
 {
     int n;
     cin>>n;
-    cout<<fact(n);
+    int arr[n];
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    f(0,arr,n);
+     for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0 ;
+   // cout<<fact(n);
 }
 
 
